2af0b3fe457f854a5c0b58b1d0f901e4bb64860f
Higuchi-Hiroyoshi <h2.hero.hh@gmail.com>
2021-03-26 16:57:39 +0900

    各deviseのコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :teachers, controllers: {
+    sessions: 'teachers/sessions',
+    passwords: 'teachers/passwords',
+    registrations: 'teachers/registrations'
+  }
+
+  devise_for :students, controllers: {
+    sessions: 'students/sessions',
+    passwords: 'students/passwords',
+    registrations: 'students/registrations'
+  }
+
   root to: "homes#index"
 end
